71cf0b3a1605b8d819e25569c2ccdf89e56eb05e
aoyangoren75183-prog <aoyangoren75183@liceo.edu.ph>
2026-07-27 20:27:32 +0800

    Remove comments from Book.java

--- src/Book.java
+++ src/Book.java
@@ -3,14 +3,12 @@ public class Book {
     private String author;
     private boolean isBorrowed;
 
-    // Constructor
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
         this.isBorrowed = false;
     }
 
-    // Getters
     public String getTitle() {
         return title;
     }
@@ -23,17 +21,14 @@ public class Book {
         return isBorrowed;
     }
 
-    // Borrow the book
     public void borrow() {
         isBorrowed = true;
     }
 
-    // Return the book
     public void returnBook() {
         isBorrowed = false;
     }
 
-    // Display book information
     public String describe() {
         String status = isBorrowed ? "Borrowed" : "Available";
         return "Title: " + title + " | Author: " + author + " | Status: " + status;
